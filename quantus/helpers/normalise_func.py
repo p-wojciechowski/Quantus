3685e1c96474962260b6af135b0e147f8d4fe128
p-wojciechowski <61585694+p-wojciechowski@users.noreply.github.com>
2023-01-18 19:38:00 +0100

    handle all-zeros cases in normalise_func.py

--- quantus/helpers/normalise_func.py
+++ quantus/helpers/normalise_func.py
@@ -5,12 +5,16 @@ import numpy as np
 
 def normalise_by_max(a: np.ndarray) -> np.ndarray:
     """ "Normalize attributions by the maximum absolute value of the explanation."""
+    if np.all(a == 0.0):
+        return a
     a /= np.max(np.abs(a))
     return a
 
 
 def normalise_by_negative(a: np.ndarray) -> np.ndarray:
     """Normalise relevance given a relevance matrix (r) [-1, 1]."""
+    if np.all(a == 0.0):
+        return a
     if a.min() >= 0.0:
         return a / a.max()
     if a.max() <= 0.0:
